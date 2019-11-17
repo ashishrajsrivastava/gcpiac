277ae812e5c4202635f929b4664c863a8bb38c9f
Ashish Raj <Ashish.Raj@emc.com>
2019-11-17 14:30:26 +0530

    deployed and tested

--- main.tf
+++ main.tf
@@ -1,10 +1,33 @@
 variable "project_id" {
-  description = "The GCP project to deploy resources"
+  description = "The GCP project ID to deploy resources"
 }
+variable "project_name" {
+  description = "The GCP project name to deploy resources"
+}
+
+variable "billing_ac_id" {
+  description = "The GCP billing account ID to be configured with project"
+}
+resource "google_project" "my_project" {
+  name       = "${var.project_name}"
+  project_id = "${var.project_id}"
+  billing_account = "${var.billing_ac_id}"
+}
+resource "google_project_service" "my_project_api" {
+  project = "${google_project.my_project.project_id}"
+  service = "compute.googleapis.com"
+
+  disable_dependent_services = true
+}
+/* provider "google" {
+  project = "${google_project.my_project.project_id}"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+} */
 resource "google_compute_instance" "default" {
-  project      = "${var.project_id}"
+  project      = "${google_project.my_project.project_id}"
   name         = "terraform"
-  machine_type = "n1-standard-1"
+  machine_type = "f1-micro"
   zone         = "us-central1-a"
 
   boot_disk {
@@ -18,6 +41,7 @@ resource "google_compute_instance" "default" {
     access_config {
     }
   }
+  depends_on = ["google_project_service.my_project_api"]
 }
 output "machinename" {
   value = "${google_compute_instance.default.name}"
